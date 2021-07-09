24ed29c7e7ec3144652ee95b09da56dbac223b8b
Liang Jinyong <Langjinyong@tinteccnc.com>
2021-07-09 17:15:31 +0900

    First Commit

---
+++ MyDotnetWeb/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyDotnetWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyDotnetWeb.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        private IStoreRepository repository;
+
+        public NavigationMenuViewComponent(IStoreRepository repo)
+        {
+            repository = repo;
+        }
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.SelectedCategory = RouteData?.Values["category"];
+            return View(repository.Products
+                .Select(p => p.Category)
+                .Distinct()
+                .OrderBy(p => p));
+        }
+    }
+}
---
+++ MyDotnetWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyDotnetWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
